a0d2ecf8010a3731f553895ef7b4bfdae057d755
Harm <vrwr4321@hotmail.com>
2018-05-08 15:41:52 +0100

    Added message implementation

--- modules/p2p/Models/message.js
+++ modules/p2p/Models/message.js
@@ -1,1 +1,8 @@
 // The dynamic message which will be send over the network
+
+function Message(content_, sender_, type_) {
+    this.content = content_;
+    this.sender = sender_;
+    this.type = type_;
+}
+  
