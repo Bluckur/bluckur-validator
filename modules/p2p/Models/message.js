8e947674ca8ba60fa138994eff23f9ed0499b124
Harm <vrwr4321@hotmail.com>
2018-05-08 15:49:08 +0100

    Changed message function to a class

--- modules/p2p/Models/message.js
+++ modules/p2p/Models/message.js
@@ -1,8 +1,9 @@
 // The dynamic message which will be send over the network
 
-function Message(content_, sender_, type_) {
-    this.content = content_;
-    this.sender = sender_;
-    this.type = type_;
+class Message {
+    constructor(content_, sender_, type_){
+        this.content = content_;
+        this.sender = sender_;
+        this.type = type_;
+    }
 }
-  
