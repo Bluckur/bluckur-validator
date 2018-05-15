3b184780bddf12aa0667fc908335257e350f1d8b
Harm <vrwr4321@hotmail.com>
2018-05-15 13:48:54 +0100

    //WIP Added the inital connector for getting the inital peer ip adress

---
+++ modules/p2p/initalconnector.js
@@ -0,0 +1,87 @@
+// Hier moet server en client zooi in gebeuren (het ontvangen en verzenden dus) eventueel kan hier ook de peer/sessie lijst bijgehouden worden.
+
+/**
+ * Default message
+ */
+module.exports = class InitialConnector {
+    /**
+     *
+     * 
+     */
+
+    constructor(firstTimeout) {
+        this.ip = "http://145.93.112.227:8082";
+        this.myIp = undefined;
+        this.peerIp = undefined;
+        this.timeout = firstTimeout;
+    }
+
+    getMyIP(){
+        return this.myIp;
+    }
+
+    getInitialPeerIP(){
+        return this.peerIp;
+    }
+
+    initate(callback){
+        this.handleRegisterIP(); // This also triggers the other calls...
+        this.callback = callback;
+    }
+
+    done(){
+        if(this.callback !== undefined){
+            this.callback();
+        }else{
+            console.log("Attempted to call callback method from initialconnector but no callback method was set");
+        }
+    }
+
+    sendRequest(path, errorImpl, successImpl){
+        var request = require('request');
+        request(this.ip + path, function (error, response, body) {
+            if(error != null || response.statusCode == 500){
+                errorImpl(error);
+            }
+            else{
+                successImpl(response, body);
+            }
+        }); 
+    }
+
+    handleRegisterIP(){
+        this.sendRequest("/register", (error) => {
+            throw "Encountered fatal error: Could not register IP from IP-serivce...";
+        }, (response, body) => {
+            console.log("Successfully registered our IP to the IP-service")
+            this.handleGetMyIP();
+        });
+    }
+
+    handleGetMyIP(){
+        this.sendRequest("/ip", (error) => {
+            throw "Encountered fatal error: Could not retreive IP from IP-serivce...";
+        }, (response, body) => {
+            this.myIp = body;
+            this.handleGetPeerIP();
+        });
+    }  
+
+    handleGetPeerIP(){
+        this.sendRequest("/", (error) => {
+            console.log("Encountered fatal error: " + error);
+        }, (response, body) => {
+            if(body === "empty"){
+                throw "Encountered fatal error: body can never be empty";
+            }
+            if(body === "first"){
+                setTimeout( () => {
+                    this.handleGetPeerIP();
+                }, this.timeout)
+            }else{
+                this.peerIp = body;
+                this.done();
+            }
+        }); 
+    }
+}
--- modules/p2p/peer.js
+++ modules/p2p/peer.js
@@ -1,54 +1,71 @@
 // Hier moet server en client zooi in gebeuren (het ontvangen en verzenden dus) eventueel kan hier ook de peer/sessie lijst bijgehouden worden.
 
+'use strict';
+
 const ioServer = require('socket.io');
-const ioClient = require('socket.io-client'),
+const ioClient = require('socket.io-client')
+const InitialConnector = require('./initalconnector.js')
 
+/**
+ * Default message
+ */
+class Peer {
     /**
-     * Default message
+     *
+     * 
      */
-    class Peer {
-        /**
-         *
-         * 
-         */
 
-        constructor() {
-            this.client = ioClient.connect('http://localhost:8000');
-            this.sequenceNumberByClient = new Map();
-            this.server = io.listen(port);
-            this.handleServer(port);
-            this.handleClient();
-        }
+    constructor() {
+        //NOTE: We need to handle our own list and cannot use the list provided by socketio. Because we now also use it to check connectivity
+        this.port = 8080;
+        this.client = ioClient.connect('http://localhost:' + this.port);
+        this.sequenceNumberByClient = new Map();
+        this.server = ioServer.listen(this.port);
 
-        handleServer(port) {
-            if (server) {
-                // event fired every time a new client connects:
-                this.server.on('connection', (socket) => {
-                    console.info(`Client connected [id=${socket.id}]`);
-                    // initialize this client's sequence number
-                    this.sequenceNumberByClient.set(socket, 1);
-                    // when socket disconnects, remove it from the list:
-                    socket.on('disconnect', () => {
-                        this.sequenceNumberByClient.delete(socket);
-                        console.info(`Client gone [id=${socket.id}]`);
-                    });
+        this.handleServer();
+        this.handleClient();
+
+        var initalconnector = new InitialConnector();
+        initalconnector.initate( () => {
+            console.log("My ip:" + initalconnector.getMyIP() + " - Peer ip: " + initalconnector.getInitialPeerIP());
+        });
+    }
+
+    handleServer() {
+        if (this.server) {
+            // event fired every time a new client connects:
+            this.server.on('connection', (socket) => {
+                console.info(`Client connected [id=${socket.id}]`);
+                // initialize this client's sequence number
+                this.sequenceNumberByClient.set(socket, 1); // ToDo: Only add if it's not a keep-alive message
+                // when socket disconnects, remove it from the list:
+                socket.on('disconnect', () => {
+                    this.sequenceNumberByClient.delete(socket);
+                    console.info(`Client gone [id=${socket.id}]`);
                 });
+                socket.on('message_isAlive', (message) => {
+                    socket.emit("message_isAlive", "Yes, I am online")
+                })
+            });
 
-                // sends each client its current sequence number
-                setInterval(() => {
-                    for (const [client, sequenceNumber] of sequenceNumberByClient.entries()) {
-                        client.emit('seq-num', sequenceNumber);
-                        sequenceNumberByClient.set(client, sequenceNumber + 1);
-                    }
-                }, 1000);
-            }
-        }
-
-        handleClient() {
-            if (this.client) {
-                this.client.on('seq-num', (msg) => console.info(msg));
-            }
+            // sends each client its current sequence number
+            // setInterval(() => {
+            //     for (const [client, sequenceNumber] of this.sequenceNumberByClient.entries()) {
+            //         client.emit('seq-num', sequenceNumber);
+            //         this.sequenceNumberByClient.set(client, sequenceNumber + 1);
+            //     }
+            // }, 1000);
         }
     }
+
+    handleClient() {
+        if (this.client) {
+            this.client.on('seq-num', (msg) => console.info(msg));
+        }
+    }
+}
+
+new Peer();
+
 
 module.exports = Peer;
