d74a67f302e248d3dc1754842c7b10eedc87c6ea
Cypher <cypher@live.nl>
2018-05-15 11:40:54 +0200

    Add Promises

--- services/database.js
+++ services/database.js
@@ -1,4 +1,5 @@
-let level = require('level');
+const level = require('level');
+
 const verbose = true;
 
 const databasePath = '../database';
@@ -7,68 +8,78 @@ const databasePath = '../database';
 let db;
 
 class Database {
-    constructor() {
-        this.open();
-    }
+  constructor() {
+    this.open();
+  }
 
-    open() {
-        this.db = level(databasePath, {createIfMissing: false}, function (err, db) {
-            if (err && verbose) console.log(err);
-        });
-    }
+  open() {
+    this.db = level(databasePath, { createIfMissing: true }, (err, db) => {
+      if (err && verbose) console.log(err);
+    });
+  }
 
-    close() {
-        this.db.close(function (err) {
-            if (err && verbose) console.log(err);
-        });
-    }
+  close() {
+    this.db.close((err) => {
+      if (err && verbose) console.log(err);
+    });
+  }
 
-    /**
+  /**
      *
      * @param {*} key
      * @param {*} value
      */
-    put(key, value) {
-        this.db.put(key, value, function (err) {
-            if (err && verbose) console.log('Unable to put ' + value + 'into the database.', err); // some kind of I/O error
-        });
-    }
+  put(key, value) {
+    return new Promise((resolve) => {
+      this.db.put(key, value, (err) => {
+        if (err && verbose) console.log(`Unable to put ${value}into the database.`, err); // some kind of I/O error
+      })
+        .then(() => { resolve(true); });
+    });
+  }
 
-    /**
+  /**
      * returns a promise
      * @param {*} key
      * @return {*} value
      */
-    get(key) {
-        return new Promise((resolve, reject) => {
-            this.db.get(key, function (err, value) {
-                if (err && verbose) return console.log(key + ' has no matches');
-                if (value) resolve(value);
-            });
-        });
+  get(key) {
+    return new Promise((resolve, reject) => {
+      this.db.get(key, (err, value) => {
+        if (err && verbose) return console.log(`${key} has no matches`);
+        if (value) resolve(value);
+      });
+    });
+  }
 
-
-    }
-
-    /**
+  /**
      *
      * @param {*} key
      */
-    delete(key) {
-        this.db.del(key, function (err) {
-            if (err && verbose) console.log(err);
-        });
-    }
+  delete(key) {
+    return new Promise((resolve) => {
+      this.db.del(key, (err) => {
+        if (err && verbose) console.log(err);
+      })
+        .then(() => { resolve(true); });
+    });
+  }
 
-    /**
-     *
-     * @param {*} ops {type: 'put/del', key:'key', value:'value'}
-     */
-    batch(ops) {
-        this.db.batch(ops, function (err) {
-            if (err && verbose) console.log(err);
-        });
-    }
+  /**
+   *
+   * @param {any} key
+   * @param {any} value
+   * @returns
+   * @memberof Database
+   */
+  update(key, value) {
+    return new Promise((resolve) => {
+      this.db.batch()
+        .del(key)
+        .put(key, value)
+        .write(() => { resolve(true); });
+    });
+  }
 }
 
 module.exports = Database;
