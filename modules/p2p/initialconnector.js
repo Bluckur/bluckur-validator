bc78621d6aa39b2850cf4a1d7d7981e9e5cb7313
Harm <vrwr4321@hotmail.com>
2018-06-05 09:28:40 +0100

    //Changed ip adress for testing

--- modules/p2p/initialconnector.js
+++ modules/p2p/initialconnector.js
@@ -14,7 +14,7 @@ module.exports = class InitialConnector {
 
     constructor(firstTimeout) {
         if (!instance) {
-            this.ip = "http://145.93.112.207:8082";
+            this.ip = "http://145.93.112.163:8082";
             this.myIp = undefined;
             this.peerIp = undefined;
             this.timeout = firstTimeout;
