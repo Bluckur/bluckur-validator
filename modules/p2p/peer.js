12536d2ae1a685bff3cd6853277f735b8095dd64
CookieAndPizza <CookieAndPizza@users.noreply.github.com>
2018-06-05 14:02:38 +0200

    Feature/29 p2p impl (#11)
    * //WIP Added the inital connector for getting the inital peer ip adress
    
    * Initial connector to promises for better async coding
    
    * some minor changes
    
    * //wip static queue import
    
    * Moved peer code to sender and receiver class
    
    * //WIP General solution for splitting client and server into receiver and sender.
    
    * added todo
    
    * // First NEW_PEER setup
    
    * Fixed method calling
    
    * Possible fix for weird connection problem
    
    * Forgot one thing, adding now
    
    * hopefully a fox
    
    * Peer is not a contructor? Stan hellap
    
    * Thanks Tu
    
    * stop adding yourself
    
    * // Fixed typo
    
    * My ip is now a fucntion
    
    * Pushed singleton to getyour own ip
    
    * fix for peerqueu
    
    * Works
    
    * Some minor fixes
    
    * Some more minor fixes
    
    * // Init connection
    
    * // console logs for flow
    
    * // equals changed to not equals
    
    * // Possible fix for multiple adding of a peer
    
    * fixed double request
    
    * Simple help respond version
    
    * Annoying undefined bug
    
    * Removed log from code
    
    * unable to add ip to queue twice
    
    * Deletefunction
    
    * FIX JO
    
    * asdklaksdjg;ashlqhef
    
    * Fixed jeroens error again
    
    * Added options to add messages to receiver and sender
    
    * /29_p2p_impl: Auto stash before merge of "feature/29_p2p_impl" and "origin/feature/29_p2p_impl"
    
    * made var of I, JEROEN....
    
    * Fixed something from jeroen. AGAIN
    
    * Added test code for sending mesages
    
    * Added final fix
    
    * Removed parts of test code
    
    * Hopefully a fix for sending unlimited messages.... Jeroen?
    
    * Fix
    
    * Added typof function to sending messages for clarity
    
    * Added myip for clarity of debugging the network
    
    * Whoepsie
    
    * Good fix for sending to own client/server
    
    * Removed ip from sending messages
    
    * Added logic for disconnection
    
    * Whoepsie
    
    * Added aditional logic for discoonection
    
    * Work in progress. Almost done...
    
    * Work in progress
    
    * Promise rejection should be gone
    
    * Made a var of toSend
    
    * Made a let of currentClient
    
    * Made a client of the getNext() method
    
    * Made ip of adress
    
    * Added + 1 to the substring
    
    * initialdisconnector has been moved to another function
    
    * removed a log
    
    * Added logs for debugging
    
    * Changed the logs
    
    * Added return to clearSockets method. Harm fked it up a bit
    
    * Added some debug souts
    
    * Added timeout to help request
    
    * Moved finished once to else
    
    * CopyQueue data fixed types
    
    * Changed the location of starting the disconnect hadndler
    
    * Added checkqueue after receiving any message
    
    * Added check for not connectiong to your own ip
    
    * Added some debug oust
    
    * Added peerqueue size sout
    
    * Removed adding client and fixed a bug
    
    * Changed tge constructor of Queue
    
    * //Changed ip adress for testing
    
    * Another minor fix
    
    * Added log for help response
    
    * Fixed sout
    
    * Added some more souts
    
    * removed all logs except one
    
    * Added sout
    
    * Possible fix for handleclienthelp
    
    * Moved addclient receives to another place
    
    * Woepsie
    
    * Added message logs once again
    
    * getNext
    
    * Added getSelfHelpRequest
    
    * Added a client iniated
    
    * Made the log less
    
    * added disconnect function
    
    * changed disconnect function
    
    * Added disconenctor to all
    
    * One cannot send a disconnected soul bakc in help request
    
    * Moved the logic of privious commit to other location
    
    * Woepsie
    
    * a
    
    * Possible fix for maximum stack call stack exceeded
    
    * Possible fix
    
    * Most likely the fix
    
    * Definetly the fix
    
    * Again a possible fix
    
    * Now you can see who you have
    
    * Added some debug souts
    
    * SOut
    
    * a
    
    * Changed osut
    
    * This is it, lets go boys
    
    * Maybe a fix
    
    * Fix
    
    * Fix2
    
    * push
    
    * Fix4
    
    * I forgot to actually send the message
    
    * Fix5
    
    * Fix6
    
    * Fix 7
    
    * Fix 8
    
    * Changed max to 3 for testing purposes
    
    * Changed mazx back to 4
    
    * Fix for disconnection thign
    
    * Uuid is now defined
    
    * This is your final boarding call.
    
    * Added.readme file

--- modules/p2p/peer.js
+++ modules/p2p/peer.js
@@ -1,54 +1,102 @@
 // Hier moet server en client zooi in gebeuren (het ontvangen en verzenden dus) eventueel kan hier ook de peer/sessie lijst bijgehouden worden.
 
+'use strict';
+
 const ioServer = require('socket.io');
-const ioClient = require('socket.io-client'),
+const ioClient = require('socket.io-client')
+const InitialConnector = require('./initialconnector.js')
+const Queue = require('./Models/queue.js')
+const Sender = require('./sender');
+const Receiver = require('./receiver');
+const Disconnector = require('./disconnector')
+const uuid = require('uuid/v1');
 
+/**
+ * Default message
+ */
+// const PQueue = new Queue(4); // Defines a queue for peers to save locally. 4 is maximum size.
+
+let instance;
+
+module.exports = class Peer {
     /**
-     * Default message
+     *
+     * 
      */
-    class Peer {
-        /**
-         *
-         * 
-         */
-
-        constructor() {
-            this.client = ioClient.connect('http://localhost:8000');
-            this.sequenceNumberByClient = new Map();
-            this.server = io.listen(port);
-            this.handleServer(port);
-            this.handleClient();
+    constructor() {
+        //NOTE: We need to handle our own list and cannot use the list provided by socketio. Because we now also use it to check connectivity
+        if (!instance) {
+            instance = this;
+            this.PeerQueue = new Queue(4);
+            this.port = 8080;
+            new InitialConnector().sleeping = false;
         }
 
-        handleServer(port) {
-            if (server) {
-                // event fired every time a new client connects:
-                this.server.on('connection', (socket) => {
-                    console.info(`Client connected [id=${socket.id}]`);
-                    // initialize this client's sequence number
-                    this.sequenceNumberByClient.set(socket, 1);
-                    // when socket disconnects, remove it from the list:
-                    socket.on('disconnect', () => {
-                        this.sequenceNumberByClient.delete(socket);
-                        console.info(`Client gone [id=${socket.id}]`);
-                    });
-                });
+        return instance;
+    }
 
-                // sends each client its current sequence number
-                setInterval(() => {
-                    for (const [client, sequenceNumber] of sequenceNumberByClient.entries()) {
-                        client.emit('seq-num', sequenceNumber);
-                        sequenceNumberByClient.set(client, sequenceNumber + 1);
-                    }
-                }, 1000);
-            }
-        }
+    initiate() {
+        this.server = ioServer.listen(this.port);
+        this.sender = new Sender(this.server, this.receiver, this.PeerQueue);
+        this.disconnector = new Disconnector(this.sender, this.server, this.PeerQueue, this);
+        this.receiver = new Receiver(this.server, this.PeerQueue, this.disconnector);
+        this.receiver.setSender(this.sender);
+        this.sender.setReceiver(this.receiver);
+        this.PeerQueue.setReceiver(this.receiver);
+        this.disconnector.setReceiverDisconnectImpl(this.receiver);
 
-        handleClient() {
-            if (this.client) {
-                this.client.on('seq-num', (msg) => console.info(msg));
+        // this.addMessageHandler("type", (message) => {
+        //      console.log(message.content);
+        //      console.log(this.PeerQueue.clearSockets())
+        // })
+
+        this.startInitialConnector();
+    }
+
+    initated(){
+        return this.PeerQueue.size() > 0;
+    }
+
+    startInitialConnector(){
+        var initalconnector = new InitialConnector(2000); //Check every 2 secs for other peer when you are first peer.
+        initalconnector.initiate().then((result) => {
+            if (result.peerIp === "first") {
+                new InitialConnector().sleeping = true
+                this.waitTillConnection()
+            } else if (result.peerIp !== "first") {
+                this.sender.sendNewPeerRequest(result.myIp, result.peerIp);
             }
+        }, (err) => {
+            // console.log(err);
+        })
+    }
+
+    // testSend() {
+    //     setTimeout(() => {
+    //         this.sendMessage("type", {
+    //             content: "hihi"
+    //         })
+    //         this.testSend();
+    //     }, 5000);
+    // }
+
+    waitTillConnection() {
+        if (new InitialConnector().sleeping) {
+            setTimeout(() => {
+                console.log("Waiting for first connection...")
+                this.waitTillConnection()
+            }, 2000)
         }
     }
 
-module.exports = Peer;
+    sendMessage(messageType, message) {
+
+        message.type = messageType;
+        message.id = uuid();
+        this.sender.sendMessageToAll(message);
+    }
+
+    addMessageHandler(messageType, implementation) {
+        this.receiver.addReceiveImplementation(messageType, implementation);
+    }
+}
--- modules/p2p/sender.js
+++ modules/p2p/sender.js
@@ -1,1 +1,72 @@
 //Handle all messages to send, regardless of them being form the 'server' or 'client'
+const InitialConnector = require('./initialconnector.js')
+const ioClient = require('socket.io-client')
+const Peer = require('./peer')
+
+class Sender {
+    constructor(ioServer, receiver, PeerQueue) {
+        this.server = ioServer;
+        this.receiver = receiver;
+        this.PeerQueue = PeerQueue;
+        this.helpRequesterStarted = false;
+
+        //Example server send:
+        //socket will come from the Queue
+        //socket.emit("message_isAlive", "Yes, I am online")
+
+        //Example client send:
+        //client.emit('seq-num', sequenceNumber);        thisConnector = this;
+
+
+        //Only add connections to queue which are made by sender...
+
+        //ToDo: NSSocket??
+
+        this.disconnectedIP = [];
+    }
+
+    setReceiver(receiver) {
+        this.receiver = receiver;
+    }
+
+    sendNewPeerRequest(myIp, peerIp) {
+        console.log(myIp + " - " + peerIp)
+        var client = ioClient.connect('http://' + peerIp + ':8080');
+        this.receiver.addClientReceives(client);
+        client.emit("new_connection", myIp);
+    }
+
+    sendHelpRequest() {
+        var self = this;
+        if (self.PeerQueue.size() < 3) {
+            if (!this.helpRequesterStarted) {
+                this.helpRequesterStarted = true;
+                setTimeout(() => {
+                    let toSend = {
+                        ip: new InitialConnector().MyIP(),
+                    }
+                    if (self.disconnectedIP) {
+                        toSend.disconnectedIP = self.disconnectedIP;
+                    }
+
+                    let currentClient = self.PeerQueue.getNext();
+                    if (currentClient) {
+                        currentClient.emit("help_request", toSend);
+                        this.helpRequesterStarted = false;
+                        self.sendHelpRequest()
+                    }
+                }, 5000)
+            }
+        } else {
+            self.helpRequesterStarted = false;
+        }
+    }
+
+    sendMessageToAll(message) {
+        this.PeerQueue.data.forEach(element => {
+            element.client.emit('message', message);
+        });
+    }
+}
+
+module.exports = Sender;
