6307881a5ba699006b313582fec21b9766881acf
Harm <vrwr4321@hotmail.com>
2018-06-05 13:09:36 +0100

    Changed max to 3 for testing purposes

--- modules/p2p/Models/queue.js
+++ modules/p2p/Models/queue.js
@@ -5,7 +5,7 @@ const ioClient = require('socket.io-client')
 
 module.exports = class Queue {
     constructor(length, data) {
-        this.max = 4;
+        this.max = 3;
         this.data = [];
         if (data !== undefined && data !== null) {
             data.forEach(element => {
