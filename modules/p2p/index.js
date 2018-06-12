62f07aceae79b3240608da635f8225495f6183a3
Stan van der Veluten <Stanneman95@gmail.com>
2018-06-12 10:54:52 +0200

    chat demo

--- modules/p2p/index.js
+++ modules/p2p/index.js
@@ -1,3 +1,17 @@
 const Peer = require('./peer')
+const readlineSync = require('readline-sync');
+
 let peer = new Peer();
 peer.initiate();
+while (!peer.initiated){
+
+}
+peer.addbroadcastMessageHandler("chat", (message) => {
+	console.log(message)
+})
+
+while (true) {
+	let message = readlineSync.question('toSend:');
+	//console.log('send ' + message + '!');
+	peer.broadcastMessage("chat", message)
+}
