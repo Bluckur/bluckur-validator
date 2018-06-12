55512862f38e8cca87eb866754744848550c823e
Harm <vrwr4321@hotmail.com>
2018-06-12 10:55:01 +0100

    Added final changes

--- modules/p2p/initialconnector.js
+++ modules/p2p/initialconnector.js
@@ -14,7 +14,7 @@ module.exports = class InitialConnector {
 
     constructor(firstTimeout) {
         if (!instance) {
-            this.ip = "http://145.93.112.163:8082";
+            this.ip = "http://192.168.1.4:8082";
             this.myIp = undefined;
             this.peerIp = undefined;
             this.timeout = firstTimeout;
--- modules/p2p/peer.js
+++ modules/p2p/peer.js
@@ -45,7 +45,7 @@ module.exports = class Peer {
         this.PeerQueue.setReceiver(this.receiver);
         this.disconnector.setReceiverDisconnectImpl(this.receiver);
 
-        this.addMessageHandler("type", (message) => {
+        this.addbroadcastMessageHandler("type", (message) => {
              console.log(message.content);
              console.log(this.PeerQueue.clearSockets())
         })
