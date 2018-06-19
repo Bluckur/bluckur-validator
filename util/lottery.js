5a3f556601fd74361b157eff1a53571e3fb18b64
Vidovitsch <david.de.bekker@gmail.com>
2018-06-19 13:07:15 +0200

    fix

--- util/lottery.js
+++ util/lottery.js
@@ -4,57 +4,56 @@ const seedrandom = require('seedrandom');
 const Database = require('bluckur-database').getInstance(process.env.IS_BACKUP === 'true');
 
 class Lottery {
-    /**
+  /**
      * Method to pick the winning block
      * @param {Array} receivedBlocks [The received blocks that need to participate into the lottery]
      * @param {String} previousBlockId [The hash / id of the last block in the ledger]
      * @param {GlobalStateUser} globalStateUsers [List of all the global state users from the Database]
      */
-    pickWinner(receivedBlocks, previousBlockId, globalStateUsers) {
-        const stakeHashMap = new HashMap();
+  pickWinner(receivedBlocks, previousBlockId, globalStateUsers) {
+    const stakeHashMap = new HashMap();
 
-        globalStateUsers.map((globalStateUser) => {
-            stakeHashMap.set(globalStateUser.publicKey, globalStateUser.stake);
-        });
+    globalStateUsers.map((globalStateUser) => {
+      stakeHashMap.set(globalStateUser.publicKey, globalStateUser.stake);
+    });
 
-        const candidatesHashMap = new HashMap();
-        const candidateBlocksHashMap = new HashMap();
+    const candidatesHashMap = new HashMap();
+    const candidateBlocksHashMap = new HashMap();
 
-        let tickets = 0;
+    let tickets = 0;
 
-        receivedBlocks.forEach((block) => {
-            const validator = block.blockHeader.validator;
-            if (stakeHashMap.has(validator)) {
-                tickets += stakeHashMap.get(validator);
-                candidatesHashMap.set(validator, stakeHashMap.get(validator));
-                candidateBlocksHashMap.set(validator, block);
-            } else {
-              Database.putStateAsync(Models.createStakeInstance({
-                publicKey: validator,
-                coin: 0,
-                stake: 0,
-              }));
-            }
-        });
+    receivedBlocks.forEach((block) => {
+      const validator = block.blockHeader.validator;
+      if (stakeHashMap.has(validator)) {
+        tickets += stakeHashMap.get(validator);
+        candidatesHashMap.set(validator, stakeHashMap.get(validator));
+        candidateBlocksHashMap.set(validator, block);
+      } else {
+        Database.putStateAsync(Models.createStakeInstance({
+          publicKey: validator,
+          coin: 0,
+          stake: 0,
+        }));
+      }
+    });
 
-        const randomNumber = seedrandom(previousBlockId)();
+    const randomNumber = seedrandom(previousBlockId)();
 
-        let bottomMargin = 0;
-        let upperMargin = 0;
-        let candidatePointer = 0;
+    let bottomMargin = 0;
+    let upperMargin = 0;
+    let candidatePointer = 0;
 
-        while (upperMargin < randomNumber) {
-            const publicKey = candidatesHashMap.keys()[candidatePointer];
-            const stake = candidatesHashMap.get(publicKey);
-            upperMargin += stake;
-            if (bottomMargin <= randomNumber && upperMargin >= randomNumber) {
-                return candidateBlocksHashMap.get(publicKey);
-            }
-            bottomMargin += stake;
-            candidatePointer += 1;
-        }
-        return Models.createBlockInstance({});
+    while (upperMargin < randomNumber) {
+      const publicKey = candidatesHashMap.keys()[candidatePointer];
+      const stake = candidatesHashMap.get(publicKey);
+      upperMargin += stake;
+      if (bottomMargin <= randomNumber && upperMargin >= randomNumber) {
+        return candidateBlocksHashMap.get(publicKey);
+      }
+      bottomMargin += stake;
+      candidatePointer += 1;
     }
+    return Models.createBlockInstance({});
   }
 }
 
